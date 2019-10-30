3ffe9f7d923ecbb64735dee30bd803e06fc3ea14
adamjmcgrath <adamjmcgrath@gmail.com>
2019-10-30 20:37:20 +0000

    - Add login page to the example app

--- examples/protected-route/src/App.js
+++ examples/protected-route/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import { useAuth0 } from 'use-auth0';
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
   return (
-    <div>
-      App
+    <div className='App'>
+      <header>
+        <NavBar />
+      </header>
     </div>
   );
 }
